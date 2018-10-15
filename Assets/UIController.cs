09482e60859cabb33a26d3bcfbb1c294e612021f
Willy Angel <gguillermix@gmail.com>
2018-10-15 21:58:39 +0200

    UI rimlight toggle

---
+++ Assets/UIController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour {
+
+	// Use this for initialization
+	public Material[] mats;
+	public Text onOffText;
+	private float onOff = 1f;
+	void Start () {
+		setMaterials();
+		onOffText.text = "ON";
+		onOffText.color = Color.green;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q)) {
+			toggleRimlight();
+		}
+	}
+
+	private void toggleRimlight() {
+		onOff = 1f - onOff;
+		if (onOff == 1f) {
+			onOffText.text = "ON";
+			onOffText.color = Color.green;
+		}
+		else {
+			onOffText.text = "OFF";
+			onOffText.color = Color.red;
+		}
+		setMaterials();
+	}
+	private void setMaterials() {
+		for (int i = 0; i < mats.Length; i++) {
+			mats[i].SetFloat("_Enablerimlight", onOff);
+		}
+	}
+}

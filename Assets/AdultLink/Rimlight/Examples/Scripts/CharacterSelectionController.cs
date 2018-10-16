2cf6c3c17d89aebb0abb6fc3fd2cfa4ca064d4b3
Willy Angel <gguillermix@gmail.com>
2018-10-16 16:03:32 +0200

    Fancy editor

--- Assets/AdultLink/Rimlight/Examples/Scripts/CharacterSelectionController.cs
+++ Assets/AdultLink/Rimlight/Examples/Scripts/CharacterSelectionController.cs
@@ -11,7 +11,7 @@ public class CharacterSelectionController : MonoBehaviour {
 
 	public void select() {
 		for (int i = 0; i < mats.Length; i++) {
-			mats[i].SetFloat("_Pulse", 1f);
+			mats[i].SetFloat("_Pulsate", 1f);
 			mats[i].SetFloat("_Rimlightopacity", 1f);
 		}
 		_light.SetActive(true);
@@ -20,7 +20,7 @@ public class CharacterSelectionController : MonoBehaviour {
 
 	public void deSelect() {
 		for (int i = 0; i < mats.Length; i++) {
-			mats[i].SetFloat("_Pulse", 0f);
+			mats[i].SetFloat("_Pulsate", 0f);
 			mats[i].SetFloat("_Rimlightopacity", 0f);
 		}
 		_light.SetActive(false);
